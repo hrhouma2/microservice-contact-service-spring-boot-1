fc5d0fa5f424e5b551b2de00ba0fb07c1e73f944
hrhouma2 <haythemrehouma@gmail.com>
2025-11-22 22:14:37 -0500

    fix: Suppression imports Thymeleaf inutilisés

--- src/main/java/com/contactservice/api/service/EmailService.java
+++ src/main/java/com/contactservice/api/service/EmailService.java
@@ -9,8 +9,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
